76cb1e89d53d634c322506a2466a73cc42f0bd56
MarChmiellio <michaelchmiel9301@gmail.com>
2026-09-13 16:22:05 -0500

    Update Module 3&4 Q1.py

--- Module 3&4 Q1.py
+++ Module 3&4 Q1.py
@@ -2,8 +2,8 @@
 # This program asks the user for two exam scores.
 # Exam 1 is worth 60% and Exam 2 is worth 40%.
 
-exam1 = float(imput("Enter the first exam score: "))
-exam2 = float(imput("Enter the second exam score: "))
+exam1 = float(input("Enter the first exam score: "))
+exam2 = float(input("Enter the second exam score: "))
 
 # Calculate the weighted total score
 total_score = (exam1 * 0.60) + (exam2 * 0.40)
