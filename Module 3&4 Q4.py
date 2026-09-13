cf443e21013b44199852137911e61e17e9cbe0a0
MarChmiellio <michaelchmiel9301@gmail.com>
2026-09-13 17:35:48 -0500

    Update Module 3&4 Q4.py

--- Module 3&4 Q4.py
+++ Module 3&4 Q4.py
@@ -1,1 +1,9 @@
+# Problem 4: Job Payment
+#This program asks for the total amount earned form a job.
+# The money is split evenly between three people.
 
+total_amount = float(input("Enter the total amount recieved for the job: $"))
+
+amount_per_person = total_amount / 3
+
+print(f"Each person will recieve: ${amount_per_person:.2f}")
