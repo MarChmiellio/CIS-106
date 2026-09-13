1fa8f64e3ae4ee9b14e21f17887dda941c5d9262
MarChmiellio <michaelchmiel9301@gmail.com>
2026-09-13 16:37:47 -0500

    Clean up comments in stock investment script
    Removed comments for clarity.

--- Module 3&4 Q2.py
+++ Module 3&4 Q2.py
@@ -6,9 +6,7 @@ ticker = input("Enter the stock ticker symbol: ")
 shares = int(input("Enter the number of shares: "))
 cost_per_share = float(input("Enter the cost per share: $"))
 
-# Calculate the total amount invested
 amount_invested = shares * cost_per_share
 
-#Display the result
 print(f"Stock ticker: {ticker}")
 print(f"Amount invested: ${amount_invested:.2f}")
