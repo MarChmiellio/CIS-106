b12dbc0ada1da9d45f12e1f4a7cddd8767e54e3d
MarChmiellio <michaelchmiel9301@gmail.com>
2026-09-13 17:46:49 -0500

    Create Module 3&4 Q5.py

---
+++ Module 3&4 Q5.py
@@ -0,0 +1,11 @@
+# Problem 5: Stock gain or loss
+# This program asks for the original purchase price per share, the current stock price, and the number of shares owned.
+# Calculates how much the investement has increased or decreased.
+
+purchase_price = floalt(input("Enter the purchase price per share: $"))
+current_price = float(input("Enter the current stock price per share: $"))
+qunatity = int(input("Enter the number of shares: "))
+
+gain_or_loss = (current_price - purchase_price) * quantity
+
+print(f"Increase/decrease in stock value: ${gain_or_loss:.2f}")
