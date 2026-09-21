c05ae9a0580c3ac5227b1aeb2681d4c1ebb8699a
MarChmiellio <michaelchmiel9301@gmail.com>
2026-09-20 22:55:57 -0500

    Add principal and years input for interest calculation

---
+++ Module 5&6 Q4.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter principal amount: "))
+years = int(input("Enter years to maturity: "))
+if principal > 100000 and years == 5:
+  interest_rate = 0.06
+elif principal >= 50000 and principal <= 100000 and years == 10:
+  interest
+  
