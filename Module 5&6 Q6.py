4359619a74bbd9f20434155cb2a4c42d582fafb5
MarChmiellio <michaelchmiel9301@gmail.com>
2026-09-20 23:27:42 -0500

    Add bonus calculation based on job level

---
+++ Module 5&6 Q6.py
@@ -0,0 +1,12 @@
+last_name = input("Enter employee last name: ")
+salary = float(input("Enter salary: "))
+job_level = int(input("Enter job level: "))
+if job_level >= 10:
+  bonus_rate = 0.25
+elif job_level >= 5:
+  bonus_rate = 0.20
+else:
+  bonus_rate = 0.10
+bonus = salary * bonus_rate
+print("Employee Last Name:", last_name)
+print(f"Bonus: ${bonus:.2f}")
