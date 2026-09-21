aa0a2c2baa62ece4dd9218cdd4f2d97183264ceb
MarChmiellio <michaelchmiel9301@gmail.com>
2026-09-20 22:21:05 -0500

    Create Module 5&6 Q1.py

---
+++ Module 5&6 Q1.py
@@ -0,0 +1,12 @@
+quantity = int(input("Enter quantity: "))
+if quantity >= 1000:
+  unit_price = 3.00
+else:
+  unit_price = 5.00
+extended_price = quantity * unit_price
+tax = extended_price * .07
+print("Quantity:", uantity)
+print(f"Unit Price: ${unit_price:.2f}")
+print(f"Extended Price: ${extended_price:.2f}")
+print(f"Tax: ${tax:.2f}")
+print(f"Total: ${total:.2f}:)
